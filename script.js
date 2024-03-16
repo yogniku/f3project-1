37f1e0e378de96b1aed3d989a803269c51765f43
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-03-16 13:30:31 +0530

    add

---
+++ script.js
@@ -0,0 +1,25 @@
+let count=0;
+document.getElementById("message")
+document.getElementById("decrement-btn").onclick=function name(params) {
+    count-=0;
+    if(count=0) {
+        console.log("Error:cannot go below 0");
+    } 
+    else {
+        console.log(" ")
+        message.classList.remove('hide')
+    }
+   // message.classList.remove('hide')
+   // document.getElementById("countlabel").innerHTML=count;
+}
+document.getElementById("increment-btn").onclick=function name(params) {
+    count+=1;
+    document.getElementById("countlabel").innerHTML=count; 
+    message.classList.add('hide')
+}
+document.getElementById("clear-btn").onclick=function name(params) {
+    count=0;
+    document.getElementById("countlabel").innerHTML=count;  
+    message.classList.remove('hide')
+}
+
